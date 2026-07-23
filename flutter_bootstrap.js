fb3db40d2f9519b1a66b6185571a33d85f9b8bfe
Mithun <mithun.s2004tumkur@gmail.com>
2026-07-23 19:23:12 +0530

    Deploy: fix Gold membership not loading after tab switch/reload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "736188573" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3897490686" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
